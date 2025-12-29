fdc6f9ef20446408735d7858ec8a3ccf9aefcf16
dangnguyenpq <nguyenhaidang11081997@gmail.com>
2025-12-29 13:22:40 +0900

    Lesson2 HelloWorld

---
+++ Lesson 2 HelloWorld/HelloWorld/HelloWorldCmd.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows;
+
+namespace HelloWorld
+{
+    [Transaction(TransactionMode.Manual)]
+    public class HelloWorldCommand : IExternalCommand
+    {
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            // Display a simple message box
+            MessageBox.Show("Hello world","Revit API 2024", MessageBoxButton.OK,MessageBoxImage.Information);
+            return Result.Succeeded;
+        }
+    }
+}
